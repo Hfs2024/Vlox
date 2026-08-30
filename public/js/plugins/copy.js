3973ad08da3d842f2f0f5fded59d5fede1586f01
Hfs2024 <seifahmedcoder@gmail.com>
2026-08-30 13:47:32 +0300

    Fix text type check in copy function and update documentation

--- public/js/plugins/copy.js
+++ public/js/plugins/copy.js
@@ -1,10 +1,18 @@
+/**
+ * Copies some text
+ * @param {string} text - Provided text to copy 
+ * @param {Function} onSuccess - Action triggered when text is successfully copied
+ * @param {Function} onFailure - Action triggered on failure
+ * @returns {boolean} Returns true if successfully copied, and false if it fails
+ */
+
 NS.copy = async ({
     text,
     onSuccess,
     onFailure
 }) => {
+    if (typeof text === "string") return false;
     if (!text) return false;
-    if (typeof text !== "string") return false;
 
     try {
         await navigator.clipboard.writeText(text);
